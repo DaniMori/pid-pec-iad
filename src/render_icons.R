ed4be97eae9b1f69dd3948e9ed57e8cf700bc5d0
DaniMori <DaniMori@users.noreply.github.com>
2024-10-02 20:26:49 +0200

    Add script for rendering email validation icons

---
+++ src/render_icons.R
@@ -0,0 +1,4 @@
+library(fontawesome)
+
+fa_png("check", file = "www/valid.png",   fill = "#49e11e", width = 40L, height = 40L)
+fa_png("xmark", file = "www/invalid.png", fill = "red",     width = 40L, height = 40L)
