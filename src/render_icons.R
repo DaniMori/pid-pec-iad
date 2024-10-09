8a82daaa3b560e7bb55d76dc61509711d479f6ea
DaniMori <DaniMori@users.noreply.github.com>
2024-10-09 21:04:20 +0200

    Clean up unnecessary files

--- src/render_icons.R
+++
@@ -1,4 +0,0 @@
-library(fontawesome)
-
-fa_png("check", file = "www/valid.png",   fill = "#49e11e", width = 30L, height = 30L)
-fa_png("xmark", file = "www/invalid.png", fill = "red",     width = 30L, height = 30L)
