af951ab5e7055d4898e3ed868eb2704e3cfd388d
DaniMori <DaniMori@users.noreply.github.com>
2024-10-14 16:19:15 +0200

    Tries methods of non-interactive authentication

--- apps/googlesheets_test.R
+++ apps/googlesheets_test.R
@@ -2,18 +2,54 @@
 # https://shiny.posit.co/r/articles/build/persistent-data-storage/
 
 library(shiny)
+library(googlesheets4)
+
 
 # Define the fields we want to save from the form
 fields <- c("name", "used_shiny", "r_num_years")
 
-library(googlesheets4)
+# Non-interactive auth setup:
+# https://gargle.r-lib.org/articles/non-interactive-auth.html#project-level-oauth-cache
 
-# Authentication steps (may be wrong or incomplete):
-# gs4_auth() # Authenticates interactively through the browser
+## Step 1: Obtain token and cache in hidden directory in current project
+## (do once per project)
+
+# designate project-specific cache
+# options(gargle_oauth_cache = here::here(".secrets"))
+
+# trigger auth on purpose --> store a token in the specified cache
+# gs4_auth()
+
+## Step 2: Announce cache location and pre-authorize token use
+## (do once per script/service; maybe in ".Rprofile"?)
+
+# Service account token setup:
+# https://gargle.r-lib.org/articles/non-interactive-auth.html#provide-a-service-account-token-directly
+
+## First create "Service Account Token" (SAT) in Google Cloud Platform:
+## https://gargle.r-lib.org/articles/get-api-credentials.html#service-account-token
+##
+## When trying to access Google Sheets (i.e. calling `gs4_get()`), prompted with
+## "enabling the Google Sheets API"; just using the link provided and enabling
+## the API works.
+##
+## Use token:
+gs4_auth(path = ".secrets/<token_filename>.json")
+
+options(
+  gargle_oauth_cache = here::here(".secrets"),
+  gargle_oauth_email = "danielmorillo.ac@gmail.com"
+)
+
+# now "do anything" that triggers authentication
 doc_url <- "https://docs.google.com/spreadsheets/d/1jzpIEa_h9AOyNSkJU80x4IFbAKj9AOkREwHmpbcPVyM"
 file_link <- gs4_get(doc_url)
-# gs_token <- gs4_token()
-# save(gs_token, file = "dat/token.Rdata")
+
+
+# TODO: Encrypting token: https://gargle.r-lib.org/articles/managing-tokens-securely.html
+KEY_NAME   <- "PID-PEC-IAD_KEY"
+secret_key <- gargle::secret_make_key()
+
 
 saveData <- function(data) {
   # The data must be a dataframe rather than a named vector
