2e00ee9dc8ed160c013fd5bcb5027ea82f59ab68
DaniMori <DaniMori@users.noreply.github.com>
2025-11-26 17:24:46 +0100

    Compute correct responses in simulation script
    This makes the simulation script ("R/simualted_data.R") standalone and specific for this application, while the rest of the code is (in principle) more reusable.

--- R/compute_responses.R
+++ R/compute_responses.R
@@ -61,66 +61,6 @@ N_DECIMALS <- 2L   # Decimal places to use for rounding numeric results
 
 ## ---- FUNCTIONS: -------------------------------------------------------------
 
-compute_correct_responses <- function(data) {
-
-  ## Constant objects: ----
-
-  # Linear regression model objects:
-  INTERCEPT_TERM <- "(Intercept)"
-  criterion_name <- SIM_VAR_NAMES['var_1'] |> glue::backtick()
-  predictor_name <- SIM_VAR_NAMES['var_2'] |> glue::backtick()
-
-
-  ## Main: ----
-
-  # Transform `var_2` to integer to use it a "linear term" in the regression
-  data <- data |> dplyr::mutate(
-    `Satisfaccion vital` = `Satisfaccion vital` |>
-      as.character() |>
-      as.integer()
-  )
-
-  # Fit model and extract coefficients:
-  model       <- glue::glue("{criterion_name} ~ {predictor_name}") # Formula
-  fitted_model <- data         |> lm(formula = model)
-  coefficients <- fitted_model |> broom::tidy()
-
-  # Compute responses:
-
-  computed_responses <- data |> dplyr::summarize(
-    item_1 = table(`Satisfaccion vital`)[ITEM_1_LS_CAT],
-    item_2 = median(`Horas sueno promedio`),
-    item_3 = sd(`Horas sueno promedio`) |> round(N_DECIMALS),
-    item_4 = quantile(`Horas sueno promedio`, .34),
-    item_5 = boxplot.stats(`Horas sueno promedio`)$out |>
-      length() |>
-      as.logical() |>
-      as.character(),
-    item_6 = cor(`Satisfaccion vital`, `Horas sueno promedio`) |>
-      round(N_DECIMALS),
-    item_7 = item_6 |> sign() |> as.character(),
-    item_8 = coefficients |>
-      dplyr::filter(term == INTERCEPT_TERM) |>
-      dplyr::pull(estimate) |>
-      round(N_DECIMALS),
-    item_9 = coefficients |>
-      dplyr::filter(term == predictor_name) |>
-      dplyr::pull(estimate) |>
-      round(N_DECIMALS)
-  )
-
-  # Capture warning when factor levels are missing in the data (they will!)
-  suppressWarnings(
-    computed_responses <- computed_responses |> dplyr::mutate(
-      item_5  = item_5 |> forcats::fct_recode(!!!ITEM_5_VALUES),
-      item_7  = item_7 |> forcats::fct_recode(!!!ITEM_7_VALUES),
-      item_10 = item_7 |> forcats::fct_recode(!!!ITEM_10_VALUES)
-    )
-  )
-
-  computed_responses
-}
-
 get_correct_responses <- function(hash) {
 
   user_sim_data <- simulate_data(hash)
--- R/simulated_data.R
+++ R/simulated_data.R
@@ -2,7 +2,7 @@
 #
 # FILE NAME:   simulated_data.R
 # DESCRIPTION: Functionality for creating a simulated dataset for the student's
-#              personal exercise
+#              personal exercise and the exercise correct responses
 #
 # AUTHOR:      Daniel Morillo
 #
@@ -26,12 +26,52 @@ SLOPE_VAR_NAME        <- "slope"
 RELATIONSHIP_VAR_NAME <- "relationship"
 
 ## Variable data:
-SAMPLE_SIZE       <- 300:500  # Uniformly random sample size of 300-500
-VAR_1_SCORES  <-  40:100 / 10 # Possible scores in `var_1`
-VAR_2_SCORES  <-   1:  5      # Possible scores in `var_2`
+SAMPLE_SIZE  <- 300:500  # Uniformly random sample size of 300-500
+VAR_1_SCORES <-  40:100 / 10 # Possible scores in `var_1`
+VAR_2_SCORES <-   1:  5      # Possible scores in `var_2`
 
 ## Modeling variables:
 REL_LEVS <- c(-1L, 1L) # Levels for the "relationship" item response
+
+
+# Response generation objects:
+
+ITEM_1_LS_CAT <- 2L # Category "Insatisfecho" in "life satisfaction" variable
+                    #   (for item 1).
+
+## Item value labels:
+SIGN_LEVELS    <- c(-1, 0, 1) |> as.character()
+LOGICAL_LEVELS <- c(FALSE, TRUE) |> as.character()
+
+ITEM_5_LABELS <- c("No", "Sí")
+ITEM_5_VALUES <- LOGICAL_LEVELS |> setNames(ITEM_5_LABELS)
+
+ITEM_7_LABELS <- c(
+  "Las variables  tienen una relación inversa",
+  "Las variables tienen una relación nula (exactamente igual a cero)",
+  "Las variables  tienen una relación directa"
+)
+ITEM_7_VALUES <- SIGN_LEVELS |> setNames(ITEM_7_LABELS)
+
+ITEM_10_LABELS <- c(
+  paste(
+    'A menos "satisfacción vital",',
+    'mayor "promedio semanal de horas diarias de sueño"'
+  ),
+  paste(
+    'La relación entre la "satisfacción vital" y el "promedio semanal',
+    'de horas diarias de sueño" es nula (exactamente igual a cero)'
+  ),
+  paste(
+    'A más "satisfacción vital",',
+    'mayor "promedio semanal de horas diarias de sueño"'
+  )
+)
+ITEM_10_VALUES <- SIGN_LEVELS |> setNames(ITEM_10_LABELS)
+
+
+## Response configuration data:
+N_DECIMALS <- 2L   # Decimal places to use for rounding numeric results
 
 
 ## ---- FUNCTIONS: -------------------------------------------------------------
@@ -69,4 +109,68 @@ simulate_data <- function(seed) {
       )
     ) |>
     setNames(SIM_VAR_NAMES)
+}
+
+compute_correct_responses <- function(data) {
+  ## Constant objects: ----
+
+  # Linear regression model objects:
+  INTERCEPT_TERM <- "(Intercept)"
+  criterion_name <- SIM_VAR_NAMES['var_1'] |> glue::backtick()
+  predictor_name <- SIM_VAR_NAMES['var_2'] |> glue::backtick()
+
+
+  ## Main: ----
+
+  # Transform `var_2` to integer to use it a "linear term" in the regression
+  data <- data |> dplyr::mutate(`Satisfaccion vital` = `Satisfaccion vital` |>
+                                  as.character() |>
+                                  as.integer())
+
+  # Fit model and extract coefficients:
+  model       <- glue::glue("{criterion_name} ~ {predictor_name}") # Formula
+  fitted_model <- data         |> lm(formula = model)
+  coefficients <- fitted_model |> broom::tidy()
+
+  # Compute responses:
+
+  computed_responses <- data |> dplyr::summarize(
+    item_1 = table(`Satisfaccion vital`)[ITEM_1_LS_CAT],
+    item_2 = median(`Horas sueno promedio`),
+    item_3 = sd(`Horas sueno promedio`) |> round(N_DECIMALS),
+    item_4 = quantile(`Horas sueno promedio`, .34),
+    item_5 = boxplot.stats(`Horas sueno promedio`)$out |>
+      length() |>
+      as.logical() |>
+      as.character(),
+    item_6 = cor(`Satisfaccion vital`, `Horas sueno promedio`) |>
+      round(N_DECIMALS),
+    item_7 = item_6 |> sign() |> as.character(),
+    item_8 = coefficients |>
+      dplyr::filter(term == INTERCEPT_TERM) |>
+      dplyr::pull(estimate) |>
+      round(N_DECIMALS),
+    item_9 = coefficients |>
+      dplyr::filter(term == predictor_name) |>
+      dplyr::pull(estimate) |>
+      round(N_DECIMALS),
+    item_10 = item_9 |> sign() |> as.character(),
+  )
+
+  # Capture warning when factor levels are missing in the data (they will!)
+  suppressWarnings(
+    computed_responses <- computed_responses |> dplyr::mutate(
+      item_5  = item_5 |>
+        readr::parse_factor(levels = ITEM_5_VALUES) |>
+        forcats::fct_recode(!!!ITEM_5_VALUES),
+      item_7  = item_7 |>
+        readr::parse_factor(levels = ITEM_7_VALUES) |>
+        forcats::fct_recode(!!!ITEM_7_VALUES),
+      item_10 = item_10 |>
+        readr::parse_factor(levels = ITEM_10_VALUES) |>
+        forcats::fct_recode(!!!ITEM_10_VALUES)
+    )
+  )
+
+  computed_responses
 }
