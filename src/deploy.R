bf319a7fafa85a9ed831574cf810aab3d717d402
DaniMori <DaniMori@users.noreply.github.com>
2026-07-09 21:18:43 +0200

    Correct deployment script
    The entry point file needs to be "app.R" necessarily. To avoid name collisions between the two app main files, the solution is to create an "app.R" temporal file from each of them, deploy it, and then remove that temporal file.

--- src/deploy.R
+++ src/deploy.R
@@ -1,16 +1,26 @@
 library(rsconnect)
 
+file.copy("data-download.R", "app.R")
+
 deployApp(
-  appFiles = c(
+  appFiles      = c(
     "app.R",
     ".Renviron",
     ".secrets/encrypted-oauth-token.rds",
     "doc/gsheets_config.yml",
     "feedback-res/student_response_variables.csv",
-    "R"
+    "R/_disable_autoload.R",
+    "R/constants.R",
+    "R/data_storage.R",
+    "R/hash_emails.R",
+    "R/log.R",
+    "R/simulated_data.R"
   ),
+  appName       = "Data Download App",
   appPrimaryDoc = "app.R",
-  appName = "Data Download App",
-  account = "iad-psi-uned",
-  server  = "connect.posit.cloud"
+  account       = "iad-psi-uned",
+  server        = "connect.posit.cloud",
+  forceUpdate   = TRUE
 )
+
+file.remove("app.R")
